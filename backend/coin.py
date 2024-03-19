380a3a12d2bba4982af50cfaa92cbbb8c8c27184
Bryan Chan <bryanchan@astri.org>
2024-03-19 17:46:12 +0800

    HTML and processing the coin/stock data from yfinance

--- backend/coin.py
+++ backend/coin.py
@@ -1,18 +1,54 @@
+import yfinance as yf
+import ta
 import pandas as pd
-from pycoingecko import CoinGeckoAPI
-import time
-import requests
-# Create a CoinGeckoAPI client
-cg = CoinGeckoAPI()
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import datetime
 
-coins = ['bitcoin', 'ethereum', 'dopex', 'ftx-token', 'optimism',
-         'jasmycoin', 'ethereum-name-service', 'solana', 'the-sandbox','Tether','dYdX']
 
-vsCurrencies = ['usd', 'eur', 'link']
+def df_to_windowed_df_auto_dates(dataframe, n=7):
+        
+    if len(dataframe) < n + 1:
+        raise ValueError(f"DataFrame 至少需要 {n+1} 行数据。")
 
-def get_coins_prices(): 
-        cg = CoinGeckoAPI()
-        simple_price_request = cg.get_price(ids=coins, vs_currencies='usd')
-        print(simple_price_request)
-        return simple_price_request 
-    
+    X, Y, dates = [], [], []
+
+    for i in range(n, len(dataframe)):
+        window = dataframe.iloc[i-n:i+1]['Close']
+        date = dataframe.index[i]
+        if len(window) != n + 1:
+            continue
+        X.append(window[:-1].values)  
+        Y.append(window[-1])         
+        dates.append(date)            
+
+    windowed_df = pd.DataFrame(X, columns=[f'Close_{i}' for i in range(n)])
+    windowed_df['Target'] = Y
+    windowed_df['Target Date'] = dates  
+
+    return windowed_df
+
+# Fetch the data
+BTC_Ticker = yf.Ticker("BTC-USD")
+BTC_Data = BTC_Ticker.history(period="max")
+
+
+del BTC_Data["Dividends"]
+del BTC_Data["Stock Splits"]
+BTC_Data["TomorrowClose"] = BTC_Data["Close"].shift(-1)
+
+BTC_Data["PriceTrend"] = (BTC_Data["TomorrowClose"]>BTC_Data["Close"]).astype(int)
+print(len(BTC_Data))
+
+
+window=df_to_windowed_df_auto_dates(BTC_Data,7)
+print(window)
+
+
+
+print(len(BTC_Data))
+
+print(len(window))
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,13 +15,29 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light large-navbar bg-pantone-7453c">
-        Asset Management System
+      <nav className="navbar navbar-expand-lg navbar-light bg-pantone-7453c">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Asset Management System</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+
+            </ul>
+          </div>
+        </div>
       </nav>
       <TotalValueDisplay></TotalValueDisplay>
       <App_AssetForm></App_AssetForm>
       <App_AssetList></App_AssetList>
-        
+
     </div>
   );
 };
