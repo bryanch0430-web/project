000e2ea883b130570c2d3037fdca5151ff26c373
Bryan Chan <bryanchan@astri.org>
2024-03-21 18:01:47 +0800

    predict up/down

--- backend/coin.py
+++ backend/coin.py
@@ -11,13 +11,128 @@ import matplotlib.pyplot as plt
 import datetime
 
 
-setTicker = "BTC-USD"
+
+def plot_distribution(y, dataset_name):
+    unique, counts = np.unique(y, return_counts=True)
+    print(unique, counts)
+    plt.bar(unique, counts)
+    plt.title(f'Distribution of classes in the {dataset_name} set')
+    plt.xlabel('Class')
+    plt.ylabel('Frequency')
+    plt.xticks(unique, classes) 
+    plt.show()
+
+setTicker = "AAPL"
 # Fetch the data
 Ticker = yf.Ticker(setTicker)
 df = Ticker.history(period="max")
+del df["Dividends"]
+del df["Stock Splits"]
+df = df[:-1]
+
+window_size = 100
+classes = ['up','down']
+up_label = classes.index('up')
+down_label = classes.index('down')
+
+X,y=[],[]
+
+for i in range(len(df)):
+    past = df[:i+1]
+    future = df[i+1:]
+    
+
+    if(len(past)<window_size or len(future)<1):
+        continue
+    
+    past_window = past[-window_size:]
+    future_window = future[:1]
+    today_price = past_window.iloc[-1]["Close"]
+    target = future.iloc[0]["Close"]
+    if target > today_price:
+        label=up_label
+    else:
+        label=down_label
+        
+    
+    X.append(past_window.values)
+    y.append(label)
+    
+X,y=np.array(X), np.array(y)
+
+
+X_temp, X_test = np.split(X, [-round(len(X) * 0.1)])
+y_temp, y_test = np.split(y, [-round(len(X) * 0.1)])
+index = np.arange(len(X_temp))
+np.random.shuffle(index)
+train_index, val_index = np.split(index, [round(len(X) * 0.7)])
+X_train, X_val = X_temp[train_index],X_temp[val_index]
+y_train, y_val = y_temp[train_index],y_temp[val_index]
+
+X_test_up = X_test[y_test==up_label]
+X_test_down = X_test[y_test==down_label]
+minlen=min(len(X_test_up),len(X_test_down))
+
+X_test_up = X_test_up[np.random.choice(len(X_test_up), minlen, replace=False), :]
+
+X_test_down = X_test_down[np.random.choice(len(X_test_down), minlen, replace=False), :]
+X_test = np.vstack([X_test_up,X_test_down])
+y_test = np.array([up_label]*minlen+[down_label]*minlen)
+
+
+'''
+plot_distribution(y_train, 'training')
+plot_distribution(y_val, 'validation')
+plot_distribution(y_test, 'test')
+'''
+
+'''
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])),
+    tf.keras.layers.LSTM(64, return_sequences=True),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.LSTM(64),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(len(classes), activation='softmax')  
+])
+'''
+model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(len(classes), activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',  
+              metrics=['accuracy'])
+
+'''
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath='best_model.keras',  # Updated to use '.keras' extension
+    monitor='val_loss',
+    save_best_only=True
+)
+
+early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss',
+    patience=100,  
+    restore_best_weights=True
+)
+'''
+
+model.fit(X_train,tf.keras.utils.to_categorical(y_train),
+          validation_data = (X_val,tf.keras.utils.to_categorical(y_val)), 
+          #callbacks=[model_checkpoint_callback,early_stopping_callback],
+          batch_size =2048,
+          epochs=1000)
 
 
 
+'''
 
 print(df)
 
@@ -78,7 +193,7 @@ plt.plot(test_df["Close"],linewidth=2)
 plt.plot(test_df["Pedict"],linewidth=1)
 plt.legend(["Train","Close","Predict"])
 plt.show()
-
+'''
 '''
 def calculate_rsi(data, window_size):
     delta = data.diff()
